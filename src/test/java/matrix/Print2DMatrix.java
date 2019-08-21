54a74d614846f294392885601c0b26688f359248
Aarti Jangid <jangid.aarti08@gmail.com>
2019-08-21 23:37:35 +0200

    Fill  and print 2D array

--- src/test/java/matrix/Print2DMatrix.java
+++ src/test/java/matrix/Print2DMatrix.java
@@ -5,7 +5,41 @@ import java.util.stream.Stream;
 
 
 public class Print2DMatrix {
+	
+	
 	public static void main(String[] args) {
+		Print2DMatrix print2dMatrix = new Print2DMatrix();
+		print2dMatrix.printMatrix();
+		
+		print2dMatrix.drawLine();
+		
+		String LINE_SEPERATOR = System
+				.getProperty("line.separator");
+		System.out.println(LINE_SEPERATOR); // "\n"
+	}
+	
+	private void drawLine() {
+		char[][] charMatrix = new char[1][9];
+		
+		int x1 = 0, y1 = 0, x2 = 8, y2 = 0;
+		
+		System.out.println("x1 " + x1);
+		System.out.println("y1 " + y1);
+		System.out.println("x2 " + x2);
+		System.out.println("y2 " + y2);
+
+		//Vertical line
+		if (x1 == x2) {
+			for (int i = y1; i <= y2; i++) {
+				charMatrix[i][x1] = '-';
+			}
+		} else if (y1 == y2) { // horizonatal line
+			Arrays.fill(charMatrix[y1], x1, x2, '-');
+		} 
+		System.out.println(Arrays.deepToString(charMatrix));
+	}
+
+	void printMatrix() {
 		int[][] intArray = {{1,2,3},
 				{4,5,6},
 				{7,8,9}
@@ -14,4 +48,5 @@ public class Print2DMatrix {
 		System.out.println(Arrays.deepToString(intArray));
 		Stream.of(intArray).map(Arrays::toString).forEach(System.out::println);
 	}
+	
 }
